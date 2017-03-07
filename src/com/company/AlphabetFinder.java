76e2bfee5e89638cc1a0ff8d5ca35019fa0871c1
Anna <ampopov@Alexanders-MacBook-Pro.local>
2017-03-07 15:26:28 +0300

    alphabet finder in string added

---
+++ src/com/company/AlphabetFinder.java
@@ -0,0 +1,132 @@
+package com.company;
+
+import java.util.*;
+
+public class AlphabetFinder {
+    static String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+    static String LONG_STRING = "wdfvwsadmwaert55bwefxccdeddfg  abcegergeeg_________defghijklmnopqrstuvabcwx yzewerzkhgdefghijklmnopqrstuvwxyzhnkih";
+
+
+    public static void main(String[] args) {
+        System.out.println("ALPHABET = " + ALPHABET);
+        System.out.println("LONG_STRING = " + LONG_STRING);
+        boolean b = simpleTask(LONG_STRING);
+        System.out.println("b = " + b);
+        RangeHolder rh = shortestAlphabetSequenceLength(LONG_STRING);
+        System.out.println("Range = " + rh);
+        System.out.println("Seq = " + LONG_STRING.substring(rh.getStart(), rh.getEnd() + 1));
+    }
+
+    public static boolean simpleTask(String s) {
+        int alphabetIdx = 0;
+        for (int j = 0; alphabetIdx < ALPHABET.length() && j < LONG_STRING.length(); j++) {
+            if (ALPHABET.charAt(alphabetIdx) == LONG_STRING.charAt(j)) {
+                ++alphabetIdx;
+            }
+        }
+        return alphabetIdx == ALPHABET.length();
+    }
+
+    public static RangeHolder shortestAlphabetSequenceLength(String s) {
+        List<RangeHolder> ranges = new ArrayList<>();
+
+        for (int j = 0; j < s.length(); j++) {
+            if (ALPHABET.charAt(0) == s.charAt(j)) {
+                boolean createNewList = true;
+                for (RangeHolder indicesHolder : ranges) {
+                    if (indicesHolder.getCount() <= 1) {
+                        createNewList = false;
+                        break;
+                    }
+                }
+                if (createNewList) {
+                    ranges.add(new RangeHolder());
+                }
+            }
+            // older ranges start earlier
+            for (int i = ranges.size() - 1; i >= 0; --i) {
+                RangeHolder rangeHolder = ranges.get(i);
+                if (i < ranges.size() - 1 && rangeHolder.getCount() <= ranges.get(i + 1).getCount()) {
+                    if (rangeHolder.getCount() == ranges.get(i + 1).getCount()
+                            && rangeHolder.getLength() <= ranges.get(i + 1).getLength()) {
+                        // no need to spend memory for longer finished sequences
+                        ranges.remove(i + 1);
+                    } else {
+                        // no need to spend memory for longer sequences that wait for same next symbol or one of previous ones
+                        ranges.remove(i);
+                    }
+                    continue;
+                }
+                int alphabetIdx = rangeHolder.getCount();
+                if (alphabetIdx < ALPHABET.length()) {
+                    if (alphabetIdx <= 1 && ALPHABET.charAt(0) == s.charAt(j)) {
+                        rangeHolder.setStart(j);
+                    }
+                    if (ALPHABET.charAt(alphabetIdx) == s.charAt(j)) {
+                        rangeHolder.setCount(++alphabetIdx);
+                        rangeHolder.setEnd(j);
+                        rangeHolder.setCount(alphabetIdx);
+                    }
+                }
+            }
+        }
+        RangeHolder minRange = null;
+        if (!ranges.isEmpty()) {
+            if (ranges.get(0).getCount() == ALPHABET.length()) {
+                minRange = ranges.get(0);
+            }
+        }
+        return minRange;
+    }
+}
+
+class RangeHolder {
+    private int start;
+    private int end;
+    private int count;
+
+    public RangeHolder() {
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    @Override
+    public String toString() {
+        return "RangeHolder{" +
+                "start=" + start +
+                ", end=" + end +
+                ", count=" + count +
+                ", length=" + getLength() +
+                '}';
+    }
+
+
+    public int getLength() {
+        return end - start + 1;
+    }
+}
+
+
+
---
+++ src/com/company/Main.java
@@ -0,0 +1,77 @@
+package com.company;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Main {
+    class E{}
+    enum D{w, e;
+    };
+
+    public static void main(String[] args) {
+        D d = D.e;
+        E ee ;
+        String s3 = new String("lis");
+        String s4 = s3.intern();
+        String s1 = "lis";
+        String s2 = "lis";
+        System.out.println(s2.toString());
+//        StringBuilder sb;
+//        sb.append().reverse().
+
+        System.out.println("s2 == s1 = " + (s2 == s1));
+        System.out.println("s3 == s1 = " + (s3 == s1));
+        System.out.println("s4 == s1 = " + (s4 == s1));
+
+        int[][] a = {{1,2}, {3,4}};
+        int[] b = a[1];
+
+        Object o1 = a;
+        int[][] a2 = (int[][]) o1;
+//        int[] b2 = (int[]) o1;
+        System.out.println(b[1]);
+
+
+	// write your code here
+        PriorityQueue pq = new PriorityQueue();
+
+        byte s = 5;
+        s &= 2;
+
+
+
+        Comparator<String> comparator = new StringLengthComparator();
+        PriorityQueue<String> queue =
+                new PriorityQueue<String>(10, comparator);
+        queue.add("short");
+        queue.add("very long indeed");
+        queue.add("medium");
+        while (queue.size() != 0)
+        {
+            System.out.println(queue.poll());
+        }
+    }
+}
+
+
+class StringLengthComparator implements Comparator<String>
+{
+    @Override
+    public int compare(String x, String y)
+    {
+        // Assume neither string is null. Real code should
+        // probably be more robust
+        // You could also just return x.length() - y.length(),
+        // which would be more efficient.
+        if (x.length() < y.length())
+        {
+            return 1;
+        }
+        if (x.length() > y.length())
+        {
+            return -1;
+        }
+        return 0;
+    }
+}
+
